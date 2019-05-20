e3de3c609ef023de334d6ad68c1f81872e516ec2
M.Odagawa <mus.29sup.guit@gmail.com>
2019-05-21 00:30:12 +0900

    Initial commit

---
+++ lib/nano_planner_web/router.ex
@@ -0,0 +1,17 @@
+defmodule NanoPlannerWeb.Router do
+  use NanoPlannerWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/", NanoPlannerWeb do
+    pipe_through :browser
+
+    get "/", TopController, :index
+  end
+end
