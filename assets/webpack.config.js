995f63aae3e71cc9792ce3b0c067ccf870001ec5
M.Odagawa <mus.29sup.guit@gmail.com>
2019-05-21 00:50:05 +0900

    .cssは使用しないので削除

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -29,10 +29,6 @@ module.exports = (env, options) => ({
           loader: 'babel-loader'
         }
       },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
     ]
   },
   plugins: [
